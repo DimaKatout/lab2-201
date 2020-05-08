105f2bd906d5089c3978bc4e11c60fb2ac497e5f
DimaKatout <dimakatout97@gmail.com>
2020-05-08 16:44:05 +0300

    lab2 without any css

---
+++ js/app.js
@@ -0,0 +1,48 @@
+'use strict'
+var name = prompt('what is your name ?' , 'Dima');
+alert('welcome ' + name );
+var q1=prompt('do i have a cat ? ');
+console.log(q1);
+if(q1.toUpperCase()==='Y'||q1.toUpperCase()==='YES')
+{alert('you got it right ');}
+else if(q1.toUpperCase()==='N'||q1.toUpperCase()==='NO')
+{alert('you got it wrong ');}
+else 
+alert('please awnser with yes or no ');
+var q2=prompt('do i have a phobia from dogs ? ');
+console.log(q2);
+if(q2.toUpperCase()==='Y'||q2.toUpperCase()==='YES')
+{alert('you got it right ');}
+else if(q2.toUpperCase()==='N'||q2.toUpperCase()==='NO')
+{alert('you got it wrong ');}
+else 
+alert('please awnser with yes or no ');
+
+var q3=prompt('is blue my favarite color ? ');
+console.log(q3);
+if(q3.toUpperCase()==='Y'||q3.toUpperCase()==='YES')
+{alert('you got it wrong ');}
+else if(q3.toUpperCase()==='N'||q3.toUpperCase()==='NO')
+{alert('you got it right ');}
+else 
+alert('please awnser with yes or no ');
+
+
+var q4=prompt('do i own a car ? ');
+console.log(q4);
+if(q4.toUpperCase()==='Y'||q4.toUpperCase()==='YES')
+{alert('you got it wrong .. ');}
+else if(q4.toUpperCase()==='N'||q4.toUpperCase()==='NO')
+{alert('you got it right ');}
+else 
+alert('please awnser with yes or no ');
+
+
+var q5=prompt('am i a Gemeni person ? ');
+console.log(q5);
+if(q5.toUpperCase()==='Y'||q5.toUpperCase()==='YES')
+{alert('you got it right ');}
+else if(q5.toUpperCase()==='N'||q5.toUpperCase()==='NO')
+{alert('you got it wrong ');}
+else 
+alert('please awnser with yes or no ');
